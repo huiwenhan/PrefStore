49772f80241d1b9fae83a8cb90cbf8d90fe3c077
huiwenhan <huiwen_han@yahoo.com.cn>
2011-11-24 10:53:20 +0800

    changed preference.thrift

--- src/main/thrift/Preference.thrift
+++ src/main/thrift/Preference.thrift
@@ -18,22 +18,42 @@ struct Page {
   2: i64 cursor
 }
 
+struct Results {
+  # byte-packed list of i64, little-endian:
+  1: binary ids
+  2: i64 next_cursor
+  3: i64 prev_cursor
+}
+
 struct PrefResults {
   1: list<Preference> prefs
   2: i64 next_cursor
   3: i64 prev_cursor
 }
 
-exception PrefzException {
+exception PrefException {
   1: string description
 }
 
 service PreferenceService {
-  void create(1: i64 userid, 2: i64 itemid,3:double score,4:string source,5:string action,6:i32 createdate,7:i32 createtype) throws(1: PrefzException ex)
-  void destroy(1: Preference pref) throws(1: PrefzException ex)
+  void create(1: i64 user_id, 2: i64 item_id,3:string source,4:string action,5:double score,6:i32 create_date,7:i32 create_type,8:i32 status) throws(1: PrefException ex)
+  void createObject(1: Preference pref) throws(1: PrefException ex)
+ 
+  void remove(1: i64 user_id, 2:i64 item_id, 3: string source, 4: string action) throws(1: PrefException ex)
+  void removeOject(1: Preference pref) throws(1: PrefException ex)
 
-  list<Preference> read(1: i64 userid) throws(1: PrefzException ex)
+  Preference selectByUserItemSourceAndAction(1: i64 user_id, 2: i64 item_id, 3: string source, 4: string action) throws(1: PrefException ex)
   
-  list<Preference> selectPreferencesBySourcAndAction(1:string source,2:string action)throws(1: PrefzException ex)
+  list<Preference> selectByUserSourceAndAction(1: i64 user_id, 2: string source, 3: string action) throws(1: PrefException ex)
+  PrefResults selectPageByUserSourceAndAction(1: i64 user_id, 2: string source, 3: string action, 4:i64 item_cursor, 5:i32 count) throws(1: PrefException ex)
+  
+  list<Preference> selectBySourcAndAction(1: string source, 2: string action)throws(1: PrefException ex)   
+  PrefResults selectPageBySourcAndAction(1: string source, 2: string action, 3:i64 user_cursor,4:i64 item_cursor, 5:i32 count) throws(1: PrefException ex)
+  
+
+  binary selectUserIdsBySource(1:string source) throws(1: PrefException ex)
+
+  void update(1: i64 user_id, 2: i64 item_id,3:string source,4:string action,5:double score,6:i32 create_date,7:i32 create_type,8:i32 status) throws(1: PrefException ex)
+  void updateObject(1:Preference pref)throws(1: PrefException ex)
   
 }
